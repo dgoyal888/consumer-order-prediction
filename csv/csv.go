77581f2544c58b53bf8e972f77f6ae3748d1a2d9
dgoyal888 <iit2016037@iiita.ac.in>
2020-07-22 18:41:59 +0530

    initial commit

---
+++ csv/csv.go
@@ -0,0 +1,57 @@
+package csv
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type CSV struct {
+	customerID string
+	customerName string
+	cuisine string
+}
+
+func ReadCSVData(filePath string) ([]CSV,error){
+
+	csvfile, err := os.Open(filePath)
+
+	if err != nil {
+		return nil,err
+	}
+
+	csvData,err := csv.NewReader(csvfile).ReadAll()
+
+	if err != nil {
+		return nil,err
+	}
+
+	csvDataSlice := []CSV{}
+
+	for i, csv := range csvData {
+		if i == 0 {
+			continue
+		}
+		if i == 5 {
+			break
+		}
+		csv := CSV{csv[0],csv[1],csv[2]}
+		csvDataSlice = append(csvDataSlice,[]CSV{csv}...)
+	}
+
+	return csvDataSlice,nil
+}
+
+func CSVToJSON(csvData []CSV) error {
+
+	jsonData, err := json.MarshalIndent(csvData,"","    ")
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s\n",jsonData)
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"consumer-order-prediction/csv"
+	"fmt"
+)
+
+
+func main() {
+
+	csvData, err := csv.ReadCSVData("data/orderdata.csv")
+
+	if err != nil {
+		fmt.Println("Error while reading CSV data: %s",err.Error())
+	}
+
+	err = csv.CSVToJSON(csvData)
+
+	if err != nil {
+		fmt.Println("Error while converting  CSV to Json data: %s",err.Error())
+	}
+}
+
