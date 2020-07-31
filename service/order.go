e111e262a12bb61bc3810e99a14cb182969af65a
dgoyal888 <iit2016037@iiita.ac.in>
2020-07-31 11:55:56 +0530

    adding service directory

--- service/order.go
+++ service/order.go
@@ -1,1 +1,34 @@
 package service
+
+import (
+	"context"
+
+	orderpb "github.com/consumer-order-prediction/pkg/proto/orders"
+)
+
+
+
+func (s *Service) PlaceOrder(ctx context.Context, req *orderpb.PlaceOrderRequest) (*orderpb.PlaceOrderResponse, error) {
+
+
+	return nil,nil
+}
+
+func (s *Service) UpdateOrder(ctx context.Context, req *orderpb.UpdateOrderRequest) (*orderpb.UpdateOrderResponse, error) {
+
+
+	return nil,nil
+}
+
+func (s *Service) GetSpecificOrder(ctx context.Context, req *orderpb.GetSpecificOrderRequest) (*orderpb.GetSpecificOrderResponse, error) {
+
+
+	return nil,nil
+}
+
+
+func (s *Service) DeleteOrder(ctx context.Context, req *orderpb.DeleteOrderRequest) (*orderpb.DeleteOrderResponse, error) {
+
+
+	return nil,nil
+}
--- service/restaurant.go
+++ service/restaurant.go
@@ -1,1 +1,58 @@
 package service
+
+import (
+	"context"
+
+	restaurantpb "github.com/consumer-order-prediction/pkg/proto/restaurant"
+)
+
+
+func (s *Service) AddRestaurant(ctx context.Context, req *restaurantpb.AddRestaurantRequest) (*restaurantpb.AddRestaurantResponse, error) {
+
+
+	return nil,nil
+}
+
+func (s *Service) GetAllRestaurant(ctx context.Context, req *restaurantpb.GetAllRestaurantRequest) (*restaurantpb.GetAllRestaurantResponse, error) {
+
+
+	return nil,nil
+}
+
+func (s *Service) GetSpecificRestaurant(ctx context.Context, req *restaurantpb.GetSpecificRestaurantRequest) (*restaurantpb.GetSpecificRestaurantResponse, error) {
+
+
+	return nil,nil
+}
+
+
+func (s *Service) AddRestaurantItem(ctx context.Context, req *restaurantpb.AddRestaurantItemRequest) (*restaurantpb.AddRestaurantItemResponse, error) {
+
+
+	return nil,nil
+}
+
+func (s *Service) UpdateRestaurantItem(ctx context.Context, req *restaurantpb.UpdateRestaurantItemRequest) (*restaurantpb.UpdateRestaurantItemResponse, error) {
+
+
+	return nil,nil
+}
+
+func (s *Service) DeleteItem(ctx context.Context, req *restaurantpb.DeleteItemRequest) (*restaurantpb.DeleteItemResponse, error) {
+
+
+	return nil,nil
+}
+
+func (s *Service) GetAllItems(ctx context.Context, req *restaurantpb.GetAllItemsRequest) (*restaurantpb.GetAllItemsResponse, error) {
+
+
+	return nil,nil
+}
+
+
+func (s *Service) GetSpecificItem(ctx context.Context, req *restaurantpb.GetSpecificItemRequest) (*restaurantpb.GetSpecificItemResponse, error) {
+
+
+	return nil,nil
+}
