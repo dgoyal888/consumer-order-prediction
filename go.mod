d32809b5b9dc77f38f3c9e80ccdc8058246b0d0c
dgoyal888 <iit2016037@iiita.ac.in>
2020-08-03 17:26:42 +0530

    adding restaurant end points

--- go.mod
+++ go.mod
@@ -5,9 +5,10 @@ go 1.14
 require (
 	github.com/aws/aws-sdk-go v1.33.16
 	github.com/gin-gonic/gin v1.6.3
-	github.com/golang/protobuf v1.4.1
+	github.com/golang/protobuf v1.4.2
 	github.com/google/uuid v1.1.1
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/prometheus/client_golang v1.7.1
 	google.golang.org/grpc v1.27.0
 	google.golang.org/protobuf v1.25.0
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
--- service/restaurant.go
+++ service/restaurant.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/consumer-order-prediction/pkg/dynamodb"
 	restaurantpb "github.com/consumer-order-prediction/pkg/proto/restaurant"
+	"github.com/consumer-order-prediction/util"
 )
 
 type Ritem struct {
@@ -42,7 +44,7 @@ func (s *Service) AddRestaurant(ctx context.Context, req *restaurantpb.AddRestau
 		Ritems: ritemSlice,
 	}
 
-	err := dynamodb.PutItem("restaurantdemo",restaurantStruct)
+	err := AddRestaurant(restaurantStruct)
 
 	if err != nil {
 		return & restaurantpb.AddRestaurantResponse{
@@ -72,12 +74,68 @@ func (s *Service) GetAllRestaurant(ctx context.Context, req *restaurantpb.GetAll
 
 func (s *Service) GetSpecificRestaurant(ctx context.Context, req *restaurantpb.GetSpecificRestaurantRequest) (*restaurantpb.GetSpecificRestaurantResponse, error) {
 
+	restaurantId := req.RestaurantId
 
-	return nil,nil
+	restaurant, err := GetRestaurant(restaurantId)
+
+	if err != nil {
+		return nil,err
+	}
+
+	var itempbSlice []*restaurantpb.Item
+
+	for _,item := range restaurant.Ritems {
+		itempb := &restaurantpb.Item{
+			ItemId:item.ItemID,
+			Name:item.Name,
+			Price: item.Price,
+		}
+		itempbSlice = append(itempbSlice,itempb)
+	}
+
+
+	resp := &restaurantpb.GetSpecificRestaurantResponse{
+		Restaurant:&restaurantpb.Restaurant{
+			RestaurantId:restaurant.RestaurantID,
+			Name:restaurant.RestaurantName,
+			Address:restaurant.Address,
+			Items: itempbSlice,
+		},
+	}
+	return resp,nil
 }
 
 
 func (s *Service) AddRestaurantItem(ctx context.Context, req *restaurantpb.AddRestaurantItemRequest) (*restaurantpb.AddRestaurantItemResponse, error) {
+
+	restaurant,err:=GetRestaurant(req.RestaurantId)
+	if err!=nil{
+		return &restaurantpb.AddRestaurantItemResponse{},err
+	}
+
+	itemID,err := util.GenerateUUID()
+
+	if err!=nil{
+		return &restaurantpb.AddRestaurantItemResponse{},err
+	}
+
+	itemRequest := &Ritem{
+		ItemID: itemID,
+		Name:req.ItemName,
+		Price:req.Price,
+	}
+
+	restaurant.Ritems=append(restaurant.Ritems,itemRequest)
+
+	err = AddRestaurant(restaurant)
+
+	if err!=nil{
+		return &restaurantpb.AddRestaurantItemResponse{},err
+	}
+
+	return &restaurantpb.AddRestaurantItemResponse{
+		Response:"Item Added Successfully",
+	},nil
 
 
 	return nil,nil
@@ -85,25 +143,156 @@ func (s *Service) AddRestaurantItem(ctx context.Context, req *restaurantpb.AddRe
 
 func (s *Service) UpdateRestaurantItem(ctx context.Context, req *restaurantpb.UpdateRestaurantItemRequest) (*restaurantpb.UpdateRestaurantItemResponse, error) {
 
+	restaurant,err:=GetRestaurant(req.RestaurantId)
+	if err!=nil{
+		return &restaurantpb.UpdateRestaurantItemResponse{
 
-	return nil,nil
+		},err
+	}
+	itemPresent:=false
+
+	itemRequest := &Ritem{
+		ItemID:req.ItemId,
+		Name:req.ItemName,
+		Price:req.Price,
+	}
+	for i,item:=range restaurant.Ritems{
+		if item.ItemID==req.ItemId{
+			restaurant.Ritems[i]=itemRequest
+			itemPresent=true
+			break
+		}
+	}
+	if !itemPresent{
+		restaurant.Ritems=append(restaurant.Ritems,itemRequest)
+	}
+
+	err = AddRestaurant(restaurant)
+
+	if err!=nil{
+		return &restaurantpb.UpdateRestaurantItemResponse{},err
+	}
+
+	return &restaurantpb.UpdateRestaurantItemResponse{
+		Response:"Item Updated Successfully",
+	},nil
 }
 
 func (s *Service) DeleteItem(ctx context.Context, req *restaurantpb.DeleteItemRequest) (*restaurantpb.DeleteItemResponse, error) {
 
+	restaurant,err:=GetRestaurant(req.RestaurantId)
+	if err!=nil{
+		return &restaurantpb.DeleteItemResponse{},err
+	}
 
-	return nil,nil
+	itemPresent := false
+	var itemIndex int
+	for i,item:=range restaurant.Ritems{
+		if item.ItemID == req.ItemId{
+			itemIndex=i
+			itemPresent = true
+			break
+		}
+	}
+
+	if !itemPresent {
+		return &restaurantpb.DeleteItemResponse{
+			Response:"Item is not present",
+		},nil
+	}
+	restaurant.Ritems= append(restaurant.Ritems[:itemIndex], restaurant.Ritems[itemIndex+1:]...)
+
+	err= AddRestaurant(restaurant)
+
+	if err!=nil{
+		return &restaurantpb.DeleteItemResponse{},err
+	}
+
+	return &restaurantpb.DeleteItemResponse{
+		Response:"Item Deleted Successfully",
+	},nil
 }
 
 func (s *Service) GetAllItems(ctx context.Context, req *restaurantpb.GetAllItemsRequest) (*restaurantpb.GetAllItemsResponse, error) {
 
+	restaurant,err := GetRestaurant(req.RestaurantId)
 
-	return nil,nil
+	if err!=nil {
+		return &restaurantpb.GetAllItemsResponse{
+
+		},err
+	}
+
+	var itempbSlice []*restaurantpb.Item
+
+	for _,item := range restaurant.Ritems {
+		itempb := &restaurantpb.Item{
+			ItemId:item.ItemID,
+			Name:item.Name,
+			Price: item.Price,
+		}
+		itempbSlice = append(itempbSlice,itempb)
+	}
+
+	return &restaurantpb.GetAllItemsResponse{
+		Items:itempbSlice,
+	},nil
 }
 
 
 func (s *Service) GetSpecificItem(ctx context.Context, req *restaurantpb.GetSpecificItemRequest) (*restaurantpb.GetSpecificItemResponse, error) {
 
+	restaurant,err := GetRestaurant(req.RestaurantId)
 
-	return nil,nil
+	if err != nil {
+		return &restaurantpb.GetSpecificItemResponse{},err
+	}
+
+	var presentItem *Ritem
+	for _,item:=range restaurant.Ritems{
+		if item.ItemID==req.ItemId{
+			presentItem = item
+			break
+		}
+	}
+
+	item := &restaurantpb.Item{
+		ItemId:presentItem.ItemID,
+		Name:presentItem.Name,
+		Price:presentItem.Price,
+	}
+	return &restaurantpb.GetSpecificItemResponse{
+		Item:item,
+	},nil
+}
+
+func GetRestaurant(restaurantId string) (*Restaurant,error){
+
+	res, err := dynamodb.GetItem("restaurantdemo","RestaurantID",restaurantId,"RestaurantID",restaurantId,&Restaurant{})
+
+	if err != nil {
+		return &Restaurant{},err
+	}
+
+	resJSON,err := json.Marshal(res)
+
+	if err != nil {
+		return &Restaurant{},err
+	}
+
+	var restaurant *Restaurant
+	err = json.Unmarshal(resJSON, &restaurant)
+
+	if err != nil {
+		return &Restaurant{},err
+	}
+
+	return restaurant,nil
+}
+
+func AddRestaurant(restaurant *Restaurant) (error){
+
+	err := dynamodb.PutItem("restaurantdemo",restaurant)
+
+	return err
 }
