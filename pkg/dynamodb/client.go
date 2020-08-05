bf29b4a84b5ffa9e9e286e1eb7d0f364c35c06b1
dgoyal888 <iit2016037@iiita.ac.in>
2020-08-05 11:32:08 +0530

    changed dynamo to local

--- pkg/dynamodb/client.go
+++ pkg/dynamodb/client.go
@@ -4,7 +4,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 )
 
 var DynamoClient *dynamodb.DynamoDB
@@ -12,15 +11,15 @@ var DynamoClient *dynamodb.DynamoDB
 
 func NewClient() {
 
-	sess := session.Must(session.NewSession(&aws.Config{
+	/*sess := session.Must(session.NewSession(&aws.Config{
 		Region:      aws.String("us-east-1"),
 		Credentials: credentials.NewStaticCredentials("AKIA6H37YGCAUJ4VRVJB", "Ck5PyAtaT4NY7BTz3kXF2JQ3nBIS+kfkJbvCX2qz", ""),
-	}))
+	}))*/
 
-	/*sess := session.Must(session.NewSession(&aws.Config{
+	sess := session.Must(session.NewSession(&aws.Config{
 		Endpoint:aws.String("http://localhost:8000"),
 		Region:aws.String("us-east-1"),
-	}))*/
+	}))
 
 	DynamoClient = dynamodb.New(sess)
 }
